3a0d57ed7cf54c6b4fbffb401c57a058ba63cc07
1three <1_three@naver.com>
2023-06-20 16:15:35 +0900

    feat: test 패키지 생성

---
+++ src/main/java/kr/three/spring/test/controller/TestController.java
@@ -0,0 +1,23 @@
+package kr.three.spring.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.three.spring.test.dto.TestDto;
+
+@RestController
+public class TestController {
+	
+	@GetMapping("/")
+	public String hello() {
+		return "Hello world";
+	}
+	
+	@GetMapping("/test")
+	public TestDto test() {
+		TestDto dto = new TestDto();
+		dto.setName("홍길동");
+		dto.setAge(10);
+		return dto;
+	}
+}
