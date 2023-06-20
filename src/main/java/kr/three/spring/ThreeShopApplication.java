a24b959ddd10753f7c356025b38df1c687382356
1three <1_three@naver.com>
2023-06-20 14:53:28 +0900

    feat: 쇼핑몰 초기 버전

---
+++ src/main/java/kr/three/spring/ThreeShopApplication.java
@@ -0,0 +1,13 @@
+package kr.three.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreeShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreeShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/three/spring/ThreeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.three.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
